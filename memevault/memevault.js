8f77edd471701b54e6a431bbef0e2e436d381cd5
SaltShaker <SaltShaker53@users.noreply.github.com>
2017-02-04 12:33:08 -0500

    Update memevault.js

--- memevault/memevault.js
+++ memevault/memevault.js
@@ -46,7 +46,7 @@ function lock() {
 }
 function showMeme() {
 	meme.innerHTML = "<a href="http://facebook.com/yourProfile">
-  <img width = \"445\" height = \"445\" src = \"/meme/thedankestmeme.jpg\">
+        <img width = \"445\" height = \"445\" src = \"/meme/thedankestmeme.jpg\"/>
                           </a>"
 }
 function hideMeme() {
