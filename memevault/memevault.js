4b5f074b56a37ed402fa9590d99670e5d1cf0ba9
SaltShaker <SaltShaker53@users.noreply.github.com>
2017-02-04 12:58:06 -0500

    Create memevault.js

---
+++ memevault/memevault.js
@@ -0,0 +1,52 @@
+window.onload = function()
+{
+	var flash = 0;
+	var colors  = [ "red", "black"];
+	var current = 0;
+	var backgroundColors  = [ "red", "orange", "yellow", "green", "blue", "purple"];
+	var backgroundCurrent = 0;
+	var object = document.getElementById("shame");
+	var htmlObject = document.getElementById("html");
+  var time = setInterval(timer, 1000);
+	var timeLeft = 10;
+	var audio = document.getElementById("audio");
+	var rip = 0;
+	var title = document.getElementById("title");
+	var warning = document.getElementById("warning");
+    function timer ()
+    {
+		if (timeLeft != 0)
+		{
+			timeLeft = timeLeft - 1;
+			object.innerHTML = "You have " + timeLeft + " seconds to leave!";
+		}
+		else
+		{
+			if (rip != 1)
+			{
+				object.innerHTML = "I WARNED YOU! YOU SHOULD OF LEFT! YOU NEVER EVEN SHOULD OF CAME HERE IN THE FIRST PLACE IF YOU READ THE URL!";
+				title.innerHTML = "SHOULD OF LEFT!";
+        warning.outerHTML = "";
+				flash = 1;
+				time =  setTimeout(timer, 0);
+				rip = 1;
+			}
+	    }
+	}
+	setInterval(function()
+	{
+		if (flash == 1)
+		{
+			object.style.color = colors[current];
+			current = (current + 1) % colors.length;
+		}
+	}, 500);
+	setInterval(function()
+	{
+		if (flash == 1)
+		{
+			htmlObject.style.backgroundColor = backgroundColors[backgroundCurrent];
+			backgroundCurrent = (backgroundCurrent + 1) % backgroundColors.length;
+		}
+	}, 100);
+}
