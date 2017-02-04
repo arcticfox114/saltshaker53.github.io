b710f24528b264fd45345125d8340bc3cb5376f9
SaltShaker <SaltShaker53@users.noreply.github.com>
2017-02-04 13:44:38 -0500

    Update dankestmeme.js

--- meme/dankestmeme.js
+++ meme/dankestmeme.js
@@ -48,7 +48,7 @@ function showHint() {
 	hint.outerHTML = "Hint: Group Chat";
 }
 function showMeme() {
-	meme.innerHTML = "<img width = \"445\" height = \"445\" src = \"/meme/thedankestmeme.jpg\"/>"
+	meme.innerHTML = "<img width = \"445\" height = \"445\" src = \"/img/thedankestmeme.jpg\"/>"
 }
 function hideMeme() {
 	meme.innerHTML = "";
