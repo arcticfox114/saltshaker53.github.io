cf39f695858c2910f808bda4ced9675303206f0a
SaltShaker <SaltShaker53@users.noreply.github.com>
2017-02-04 12:19:31 -0500

    Add files via upload

---
+++ memevault/memevault.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+  code = "i solemnly swear i'm upto no good";
+  code_input = document.getElementById("code_input");
+  lock_status = document.getElementById("lock_status");
+  lock_button = document.getElementById("lock_button");
+  unlock_button = document.getElementById("unlock_button");
+  hint = document.getElementById("hint");
+  meme = document.getElementById("meme");
+  locked = true;
+};
+function check() {
+	if (locked)
+	{
+		if (code_input.value == code)
+		{
+      if (code_input.className == "invalid")
+      {
+        code_input.className = "";
+      }
+			unlock();
+		}
+    else
+    {
+      code_input.className = "invalid";
+    }
+	}
+}
+function unlock() {
+	lock_status.className = "lockstatus unlocked";
+	lock_status.innerHTML = "<i class = \"fa fa-unlock\"></i> Unlocked";
+	code_input.value = "";
+	code_input.disabled = true;
+	lock_button.disabled = false;
+	unlock_button.disabled = true;
+	locked = false;
+	showMeme();
+}
+function lock() {
+	lock_status.className = "lockstatus locked";
+	lock_status.innerHTML = "<i class = \"fa fa-lock\"></i> Locked";
+	code_input.disabled = false;
+	lock_button.disabled = true;
+	unlock_button.disabled = false;
+	locked = true;
+	hideMeme();
+}
+function showMeme() {
+	meme.innerHTML = "<a href="/unlocked/home/"Access the meme vault</a>"
+}
+function hideMeme() {
+	meme.innerHTML = "";
+}
